9c01ab5ab6b1a23d1064ccce2b11ea8e6adab67e
Alex Naupay <alexnaupay@gmail.com>
2025-06-28 20:32:52 -0500

    Add `@ts-ignore` comment to suppress TypeScript errors

--- src/regex-g.ts
+++ src/regex-g.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 const perspectives = "<ul><li>A corto y mediano plazo, no existen indicios asociados al desarrollo de un nuevo proceso eruptivo, por lo que el nivel de alerta volcánica se mantiene en color verde.</li></ul>";
 
 const regexx = /<li>(.*?)<\/li>/g;
